78a7d0e9d4e682e7823c14d71178c408f7f0bbe0
Adil-Kassam <kassamadil786@gmail.com>
2023-12-03 11:43:50 +0000

    Test name still exists

--- things/models.py
+++ things/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.core.exceptions import ValidationError
 
 
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(unique=True, max_length=30, blank=False)
     description = models.CharField(unique=False, max_length=120, blank=True)
-    quantity = models.IntegerField( default='0', validators = [MinValueValidator(0), MaxValueValidator(100)])
-    
+    quantity = models.IntegerField( default=0, validators = [MinValueValidator(0), MaxValueValidator(100)])
